de36928dcd1e0c96ae7b9e733a5317eb96d36bb9
tyguy89 <121373027+tyguy89@users.noreply.github.com>
2023-11-11 21:20:22 -0600

    Add files via upload

---
+++ DivingVisualTracker/main.py
@@ -0,0 +1,14 @@
+
+import global_reference, image_backend, config, image_analysis
+
+
+class DryboardVisualTracker:
+    def __init__():
+        self.id = 0
+    
+    def start():
+        id = 1
+    
+    def kill():
+        destroy()
+    
