3b3f098774b922769b1a0e54396c6d160a5434a8
tjb404 <tjb404@mail.usask.ca>
2023-11-16 23:16:27 -0600

    Large commits and developments in image and colour detection groundwork

--- DivingVisualTracker/image_analysis/arm_endpoint_tracking.py
+++ DivingVisualTracker/image_analysis/arm_endpoint_tracking.py
@@ -1,22 +1,59 @@
+#Tyler Boechler
+
 import cv2 as cv
 import numpy as np
 import sys, os
 
-sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path)
 
+def find_pixel_neighbours(img, x, y):
+    neighbours = []
+    startx = -1 
+    endx = 2
+    starty = -1 
+    endy = 2
+
+    if x + 1 >= len(img):
+        endx = 1
+
+    if x == 0:
+        startx = 0
+
+    if y + 1 >= len(img[0]):
+        endy = 1
+
+    if y == 0:
+        starty = 0
+
+    if len(neighbours) == 0:
+        for i in range(startx, endx):
+            n1 = []
+            for j in range(starty, endy):
+                #if i == 0 and j == 0:
+                   # continue
+               # else:
+                n1.append(img[x+i][y+j])
+            #print(n1)
+            neighbours.append(n1)
+    return np.array(neighbours)
 
 def find_colour_in_image(img):
     new_array = np.zeros((len(img), len(img[0]), 3), np.uint8, 'C')
+    
     counter = [0, 0]
+    np. set_printoptions(threshold=np. inf)
     for i in range(0, len(img)):
         for j in range(0, int(len(img[i]))):
+            
+                #print(img[i][j])
+
 
             local_pixel = img[i][j]
             r = local_pixel[0]
             g = local_pixel[1]
             b = local_pixel[2]
 
-            new_r = 0
+            new_r = 0 
             new_g = 0
             new_b = 0
             
@@ -34,7 +71,7 @@ def find_colour_in_image(img):
                 new_b = 0
             
             #RED DOMINANT COLOUR
-            elif r >= g and r >= b:
+            elif r >= g and r > b:
                 if g / r > 0.4:
                     if g/r > 0.75:
                         #YELLOW
@@ -114,17 +151,63 @@ def find_colour_in_image(img):
     print(counter)
     return new_array
 
-def extract_colours_in_image(path):
+def is_edge_of_shape(array):
+    id = array[1][1]
+    #print(id, "id")
+    counter = 0
+    for a in array:
+        for each in a:
+            if not np.array_equal(each, id):
+                counter += 1
+    
+    return counter >= 3 and counter <= 5
+
+def find_all_shape_edges(filtered_img):
+    colour_edges = np.zeros((len(filtered_img), len(filtered_img[0]), 3), np.uint8, 'C')
+    for i in range(len(filtered_img)):
+        for j in range(len(filtered_img[i])):
+            ninebynine = find_pixel_neighbours(filtered_img, i, j)
+            if is_edge_of_shape(ninebynine):
+                colour_edges[i][j] = filtered_img[i][j]
+
+    return colour_edges
+def extract_shapes_from_np(img, start_coord, range1, range2):
+    #Extracts shapes from a given range and area on the np image
+    #Square, Rectangle, Circle, Ellipse, Triangle
+    assert start_coord[0] + range1 < len(img) and start_coord[1] + range2 < len(img[0]), "Out of bounds"
+
+
+    
+    #Ideas for extraction : 
+    #Finding a background colour would make my life easier or sewing a giant green blanket
+    # A shape flood fill algorithm
+    # A shape perimeter / area detection with brute force analysis or simple neural network
+    # Use filters on image to get it to a less colourful noise format
+    # Rely heavily on markup of frames
+    # Add extra parameters and logic in pixel creation for all pixels that have 3 neighbours of a different colour and then keep differnt colours as seperate objects 
+
+def extract_nprgb_path_of_image(path):
     img = cv.imread(path)
     
-    #cv.imshow("Display Window", img)
-    return find_colour_in_image(img)
+
+    return img
 
 def main():
 
-    colour_object = extract_colours_in_image("python_projects/DivingVisualTracker/image_analysis/image0 (3).jpeg")
-    cv.imshow("img", colour_object)
+    #index = {"red": [255, 0, 0], "green": [0, 255, 0], "blue": [0, 0, 255], "light blue": [0, 157, 255], "cyan": [0, 255, 255], "aqua": [0, 255, 190], "pink": [255, 0, 255], "magenta": [190, 0, 255], "purple": [100, 0, 255], "light green": [150, 255, 0], "yellow": [255, 255, 0], "orange": [255, 165, 0]}
+    #reversed_index = {}
+    #for k in index:
+       # reversed_index[str(index[k])] = k
+    
+
+    colour_object = extract_nprgb_path_of_image("python_projects/DivingVisualTracker/image_analysis/image0 (4).jpeg")
+    
+    filtered_image = find_colour_in_image(colour_object)
+    edges_of_colours = find_all_shape_edges(filtered_image)
+
+    cv.imshow("img", edges_of_colours)
+
 
     cv.waitKey(0)
-    #cv.destroyAllWindows()
+    cv.destroyAllWindows()
 main()
